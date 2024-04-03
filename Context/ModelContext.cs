f73398cafaf4f7476c68096c48c2c86d612de638
Vinicius Alves Arcanjo <vinicius.vini041@gmail.com>
2024-04-03 09:35:07 -0300

    feature: adicionando criação do banco e tabelas no ModelContext

--- Context/ModelContext.cs
+++ Context/ModelContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Productify.Models;
 
 namespace Productify.Context
@@ -9,7 +11,19 @@ namespace Productify.Context
         public ModelContext(DbContextOptions<ModelContext> options)
            : base(options)
         {
-            Database.EnsureCreated();
+            try
+            {
+                var dbCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(dbCreator != null)
+                {
+                    if (!dbCreator.CanConnect()) dbCreator.Create();
+                    if (!dbCreator.HasTables()) dbCreator.CreateTables();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }    
         }
 
         public DbSet<Produto> Produtos { get; set; }
